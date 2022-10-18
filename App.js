e4af44a73d26b332b9db644b397d7da6897202ef
qoalstn44 <qoalstn44@naver.com>
2022-10-18 18:00:39 +0900

    App(1)

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>배민수</Text>
       <StatusBar style="auto" />
     </View>
   );
