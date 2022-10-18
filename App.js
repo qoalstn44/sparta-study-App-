1a4c6e42ad8b6b20ab5ebcfa4d6005510b0f3f7c
qoalstn44 <qoalstn44@naver.com>
2022-10-18 19:45:45 +0900

    App(2)

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>배민수</Text>
-      <StatusBar style="auto" />
+      <View style={styles.containerOne}>
+
+      </View>
+      <View style={styles.containerTwo}>
+        <View style={styles.innerOne}>
+         
+        </View>
+        <View style={styles.innerTwo}>
+          <View style={styles.content}></View>
+        </View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1
   },
+  containerOne: {
+    flex:1,
+    backgroundColor:"red"
+  },
+  containerTwo:{
+    flex:2,
+    flexDirection:"row",
+    backgroundColor:"yellow"
+  },
+  innerOne: {
+    flex:1,
+    backgroundColor:"blue"
+  },
+  innerTwo: {
+    flex:4,
+    backgroundColor:"orange",
+    alignItems:"flex-end"
+  },
+  content: {
+    width:50,
+    height:50,
+    backgroundColor:"#000"
+  }
 });
